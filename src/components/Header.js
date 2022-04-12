f6cda4854ff4a8f74d52e4d9bedb6e6fe08e53ed
Muhammad Ali <muhammadalii1998@gmail.com>
2022-04-12 15:00:21 +0500

    Top Header settle down + Logo Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Logo from "./../images/SatrLogo.png";
 const Header = () => {
     return(
-        <div className="w-[100%] h-[40px] bg-black">
+        <div>
+            <div className=" flex justify-center  w-[100%] h-[28px] top-header-bg">
+                <h1 className="text-white font-extrabold "> Merging Style with Elegance</h1>
+            </div>
+            <div className="flex w-[100%] h-[100px] bg-black">
+                <div className="float-left ml-1 ">
+                    <img src={Logo} alt="Logo" width="100px" />
+                </div>
+                <div>
 
+                </div>
+            </div>
         </div>
     )
 }
